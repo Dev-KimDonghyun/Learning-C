74b25d97bd6c1df1f39cc364c410253f959027da
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-21 08:57:24 +0900

    learn: Add C File for Operator Learn

---
+++ C Basic/06_operator.c
@@ -0,0 +1,16 @@
+// 2025.08.21
+// 06_operator.c
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    a = 10;
+    b = 3;
+    printf("a + b = %d \n", a + b);
+    printf("a - b = %d \n", a - b);
+    printf("a * b = %d \n", a * b);
+    printf("a / b = %d \n", a / b);
+    printf("a %% b = %d \n", a % b);
+    return 0;
+}
---
+++ C Basic/07_operator.c
@@ -0,0 +1,14 @@
+// 2025.08.21
+// 07_operator.c
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    double b;
+
+    a = 10;
+    b = 3;
+    printf("a / b = %f \n", a / b);
+    return 0;
+}
